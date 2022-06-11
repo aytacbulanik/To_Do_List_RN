c37b1be959c4854d0e8946cba2188ce00aba2e30
Aytac Bulanık <aytacbulanik@hotmail.com>
2022-06-11 12:42:42 +0300

    custom card component files added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView,View,Text} from 'react-native';
+
+const todoApp = () => {
+  return(
+    <SafeAreaView>
+      <View>
+        <Text> deneme </Text>
+      </View>
+    </SafeAreaView>
+  )
+};
+
+export default todoApp;
